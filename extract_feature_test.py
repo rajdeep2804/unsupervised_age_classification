01fc888519bd0681c5a135569146be302d6d5ade
Rajdeep Singh <53480855+rajdeep2804@users.noreply.github.com>
2020-11-12 01:31:17 +0530

    Add files via upload

--- extract_feature_test.py
+++ extract_feature_test.py
@@ -7,16 +7,17 @@ from imutils import paths
 import numpy as np
 import random
 import os
-path = r'C:\Users\hp\Pictures\dataset_unsupervised'
+path = r'C:\Users\hp\Pictures\dataset_unsupervised_test'
 feature_extraction = r'C:\Users\hp\Pictures\Food\output_dir'
-feature_vec = "feature_vec"
+feature_vec_test = "feature_vec_test"
 batch_size = 32 
 model = VGG16(weights="imagenet", include_top=False)
-csvPath = os.path.sep.join([feature_extraction, "{}.csv".format(feature_vec)])
-print(csvPath)
+csvPath = os.path.sep.join([feature_extraction, "{}.csv".format(feature_vec_test)])
 csv = open(csvPath, 'a')
 for image_paths in os.listdir(path):
     input_path = os.path.join(path, image_paths)
+    tail = os.path.split(input_path)
+    file_name = tail[1]
     image = load_img(input_path, target_size = (224, 224))
     image = img_to_array(image)
     image = np.expand_dims(image, axis=0)
@@ -25,7 +26,7 @@ for image_paths in os.listdir(path):
     features = features.reshape((features.shape[0], 7 * 7 * 512))
     for vec in features:
         vec = ",".join([str(v) for v in vec])
-        csv.write("{}\n".format(vec))
+        csv.write("{},{}\n".format(file_name,vec))
                 
     
 csv.close()
